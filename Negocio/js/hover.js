208981171d462c30d5dd62e6dc97792205ac8634
christhian-aracena <christhian.aracena@correoaiep.cl>
2024-02-20 17:05:00 -0300

    avance create.php

---
+++ Negocio/js/hover.js
@@ -0,0 +1,9 @@
+function mostrarImagenHover() {
+    var imagenHover = document.querySelector('.imagen-hover');
+    imagenHover.style.opacity = '1';
+}
+
+function ocultarImagenHover() {
+    var imagenHover = document.querySelector('.imagen-hover');
+    imagenHover.style.opacity = '0';
+}
